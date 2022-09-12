5b0fd11b0ddebfc96c38b2affd821de0cb54dba4
gaetan <gaetan.ansotte@hotmail.com>
2022-09-12 20:17:47 +0200

    clean logger

--- apps/discord-bot/src/logger.ts
+++ apps/discord-bot/src/logger.ts
@@ -1,13 +1,16 @@
 import { createLogger, transports, format } from "winston";
+const { combine, timestamp, label, printf } = format;
 
+const myFormat = printf(({ timestamp, level, message, metadata }) => {
+    return `[${timestamp.slice(0, -5)}] ${level}: ${message}. ${(Object.keys(metadata).length === 0) ? '' : JSON.stringify(metadata)}`;
+});
 export const logger = createLogger({
     transports: [
         new transports.Console({
             format: format.combine(
                 format.colorize(),
-                format.printf(({ timestamp, level, message, metadata }) => {
-                    return `[${timestamp}] ${level}: ${message}. ${(Object.keys(metadata).length === 0) ? '' : JSON.stringify(metadata)}`;
-                })
+                timestamp(),
+                myFormat
             ),
         }),
     ],
